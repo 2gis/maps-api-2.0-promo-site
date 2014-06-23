91f8a705f6362e5a909be1b3dcd7489e6d2d4881
y.voloshchuk <wenqeer@gmail.com>
2014-06-23 11:45:43 +0300

    heatmap cleanup

--- app/maps/sun.js
+++ app/maps/sun.js
@@ -2,7 +2,6 @@ var data = require('./cafeHeat');
 
 module.exports = function(map) {
   map.setView([55.696351217825494, 37.6426394592835]);
-  // map.setView([-37.8839, 175.3745188667], 7);
 
   if (map.heat) {
     return map.addLayer(map.heat);
