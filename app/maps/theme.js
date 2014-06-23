99ef61e52b5e398295524380bc53efb932c0534b
Dmitry Lutsik <otoyto@gmail.com>
2014-06-23 13:49:37 +0300

    adjust setView coords for 1366X768 screens

--- app/maps/theme.js
+++ app/maps/theme.js
@@ -2,7 +2,7 @@ var $ = require('jquery');
 require('../../polyfill/indexOf');
 
 module.exports = function(map, skin) {
-	map.setView([54.980156831455, 82.897440725094], 18);
+	map.setView([54.980496831455, 82.897140725094], 18);
 	var css = $('head [rel=stylesheet]').filter(function() {
 			return $(this).attr('href').indexOf('maps.api') > -1;
 		}),
