3c01f5ba5c82c01baeabc7036157dd593c4b0cc7
y.voloshchuk <wenqeer@gmail.com>
2014-06-23 18:23:54 +0300

    device map sync

--- app/maps/tablet.js
+++ app/maps/tablet.js
@@ -1,5 +1,11 @@
 module.exports = function(map, app) {
+    var map2 = app.device.map;
     app.device.enable().setType('tablet');
+
+    map.on('move', function(e) {
+        console.log(e.target.getCenter());
+        map2.setView(e.target.getCenter(), map.getZoom(), {animate: false});
+    });
 
     return map;
 };
