84e80175b6e71194d20eef0d1c5c04a9541729f4
y.voloshchuk <wenqeer@gmail.com>
2014-06-23 14:46:15 +0300

    modularity animated marker + modularity cleanup

--- app/maps/sun.js
+++ app/maps/sun.js
@@ -1,15 +1,13 @@
 var data = require('./cafeHeat');
 
 module.exports = function(map, app) {
-  map.setView([55.755616894047215, 37.60070800781251]);
+    if (map.heat) {
+        return map.addLayer(map.heat);
+    } else {
+        app.plugins.heat.then(function() {
+            map.heat = DG.heatLayer(data, {radius: 25}).addTo(map);
+        });
+    }
 
-  if (map.heat) {
-    return map.addLayer(map.heat);
-  } else {
-    app.plugins.heat.then(function() {
-      map.heat = DG.heatLayer(data, {radius: 25}).addTo(map);
-    });
-  }
-
-  return map;
+    return map;
 };
