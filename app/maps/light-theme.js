c7ae2ef0d5a0cd69d98c3bcbbb84d28ceb6fe9dc
y.voloshchuk <wenqeer@gmail.com>
2014-06-23 14:56:44 +0300

    major cleanup

--- app/maps/light-theme.js
+++ app/maps/light-theme.js
@@ -1,9 +1,7 @@
 var theme = require('./theme');
 
 module.exports = function(map) {
-	// map.setView([54.980156831455, 82.137440225094]);
+    theme(map, 'light');
 
-	theme(map, 'light');
-
-	return map;
+    return map;
 };
--- app/maps/sun.js
+++ app/maps/sun.js
@@ -3,11 +3,11 @@ var data = require('./cafeHeat');
 module.exports = function(map, app) {
     if (map.heat) {
         return map.addLayer(map.heat);
-    } else {
-        app.plugins.heat.then(function() {
-            map.heat = DG.heatLayer(data, {radius: 25}).addTo(map);
-        });
     }
+
+    app.plugins.heat.then(function() {
+        map.heat = DG.heatLayer(data, {radius: 25}).addTo(map);
+    });
 
     return map;
 };
