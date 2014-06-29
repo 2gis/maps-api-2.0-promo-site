9bef9fcf330091c1494047236b8cab78710cb4d8
Dmitry Lutsik <otoyto@gmail.com>
2014-06-29 18:00:12 +0300

    clean up

--- app/view/control.js
+++ app/view/control.js
@@ -91,7 +91,6 @@ module.exports = function (app) {
             this.$('.' + activeClass).removeClass(activeClass);
             $el.addClass(activeClass);
             state = $el.data('state') ? $el.data('state') : 'page' + this.model.get('page');
-            console.log(state);
             this.model.set('state', state);
 
             return this;
