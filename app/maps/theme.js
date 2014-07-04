6f4c7a1594d6ad180e8b2a0c3f5ebe6a030ccbf0
y.voloshchuk <wenqeer@gmail.com>
2014-07-04 14:32:21 +0300

    theme switcher rework

--- app/maps/theme.js
+++ app/maps/theme.js
@@ -3,18 +3,24 @@ require('../../polyfill/indexOf');
 
 module.exports = function(map, skin) {
     map.setView([59.94067812167326, 30.327420830726624], 18);
-    var css = $('head [rel=stylesheet]').filter(function() {
+    var css1 = $('head [rel=stylesheet]').filter(function() {
             return $(this).attr('href').indexOf('maps.api') > -1;
         }),
-        url = css.attr('href'),
+        css2 = css1.clone(),
+        url = css1.attr('href'),
         regex = /skin=(\w*)/,
         test = url.match(regex);
 
     map.geoclicker.enable();
 
-    map.fire('click', {latlng: new DG.LatLng(59.94017833263666, 30.32859027385712)});
+    css2.attr('href', test ? url.replace(regex, 'skin=' + skin) : (url + '&skin=' + skin));
 
-    css.attr('href', test ? url.replace(regex, 'skin=' + skin) : (url + '&skin=' + skin));
+    css2.one('load', function() {
+        map.fire('click', {latlng: new DG.LatLng(59.94017833263666, 30.32859027385712)});
+        css1.remove();
+    });
+
+    css1.after(css2);
 
     return map;
 };
