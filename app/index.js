0365ecf84f0da9d03ede7a93931af2d451e82e95
y.voloshchuk <wenqeer@gmail.com>
2014-07-11 13:59:40 +0300

    chrome on ipad viewport fix

--- app/index.js
+++ app/index.js
@@ -1,16 +1,29 @@
 window.app = {
-		plugins: {}
-	};
-var	State = require('./state')(app),
-	Router = require('./router')(app),
-	Backbone = require('backbone'),
+    plugins: {}
+};
+var State = require('./state')(app),
+    Router = require('./router')(app),
+    Backbone = require('backbone'),
     _ = require('underscore'),
-	$ = require('jquery');
+    $ = require('jquery');
 
 app.root = '/mapsapi';
 app.state = new State();
 
 $(document).ready(function () {
-	app.router = new Router();
-	Backbone.history.start({ pushState: false, root: app.root });
+    app.router = new Router();
+    Backbone.history.start({ pushState: false, root: app.root });
 });
+
+function hideAddressBar() {
+  if (!window.location.hash) {
+        if(document.height < window.outerHeight) {
+            document.body.style.height = (window.outerHeight + 50) + 'px';
+        }
+
+        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+    }
+}
+
+window.addEventListener('load', function(){ if (!window.pageYOffset) { hideAddressBar(); } });
+window.addEventListener('orientationchange', hideAddressBar);
